4e16802c1e4bffcdd8d247f5896c170307cc6254
shadowjljp <40411574+shadowjljp@users.noreply.github.com>
2020-10-06 18:03:06 -0500

    update

--- LeetCode/src/main/java/design/ZigzagIterator.java
+++ LeetCode/src/main/java/design/ZigzagIterator.java
@@ -1,9 +1,47 @@
 package design;
 
-public class ZigzagIterator {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
+public class ZigzagIterator {
+	Queue<Integer> queue;
+	public ZigzagIterator(List<Integer> v1,List<Integer> v2) {
+		queue = new LinkedList<>();
+		int i=0;
+		while(i<v1.size()  && i<v2.size()) {
+			queue.offer(v1.get(i));
+			queue.offer(v2.get(i));
+			i++;
+		}
+		while(i<v1.size()) {
+			queue.offer(v1.get(i++));
+		}
+		while(i<v2.size()) {
+			queue.offer(v2.get(i++));
+		}
+	}
+	
+	public int next() {
+		return queue.poll();
+	}
+	public boolean hasNext() {
+		return !queue.isEmpty();
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 List<Integer> v1 = new ArrayList<Integer>();
+		    List<Integer> v2 = new ArrayList<Integer>();
+		    v1.add(1);
+		    v1.add(2);
+		    v2.add(3);
+		    v2.add(4);
+		    v2.add(5);
+		    v2.add(6);
+		    ZigzagIterator i = new ZigzagIterator(v1, v2);
+		    while(i.hasNext()) {
+		        System.out.println(i.next());
+		    }
 
 	}
 
---
+++ LeetCode/src/main/java/merge/DominatePoints.java
@@ -0,0 +1,20 @@
+package merge;
+
+import java.util.ArrayList;
+
+class Pair{
+	int x;
+	int y;
+	public Pair(int x,int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+public class DominatePoints {
+	
+	public static void main(String[] args) {
+		ArrayList<Pair> l = new ArrayList<>();
+		l.add(new Pair(0,100))   ;
+
+	}
+}
